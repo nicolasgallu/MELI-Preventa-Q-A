f55ad731cf2cb844096993273e99f17588ce786b
nicolasgallu <nicolasgallu@pm.me>
2025-04-07 22:13:13 +0200

    adding new commit 1

--- auxiliar/clean_cache.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Borrando __pycache__ y archivos .pyc..."
-
-find . -type d -name "__pycache__" -exec rm -r {} + -print
-find . -type f -name "*.pyc" -delete -print
-find . -type f -name "*.pyo" -delete -print
-
-echo "Limpieza completada."
