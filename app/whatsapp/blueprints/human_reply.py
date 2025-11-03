6f654daafd9dcc33f28eefbe19c311f8cd658d7e
nicolasgallu <nicolasgallu@pm.me>
2025-11-02 22:50:58 -0300

    fixing human reply bleuprint

--- app/whatsapp/blueprints/human_reply.py
+++ app/whatsapp/blueprints/human_reply.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, make_response
 from app.shared.core.logger import logger
-from app.whatsapp.utils.webhook_init import wh_activation
+from app.whatsapp.utils.security import wh_activation,sign_validation
 from app.whatsapp.services.wpp_pipeline import pipeline
 
 wpp_bp = Blueprint("wpp", __name__, url_prefix="/wpp")
@@ -10,6 +10,10 @@ def handle_webhook():
     if request.method =='GET':
         logger.info("Processing GET Request from WPP")
         return wh_activation(request)
+    
     if request.method =='POST' and request.get_json():
-        logger.info("Processing POST Request from WPP")
-        return pipeline(request)
+        if sign_validation(request) == False:
+            return make_response('Forbidden', 403)
+        else:
+            logger.info("Processing POST Request from WPP")
+            return pipeline(request)
